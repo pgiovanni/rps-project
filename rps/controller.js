5c6879417c6d1bb9b238329651765cfaf07c38cb
Paul Giovanni <pgiovanni1234@gmail.com>
2022-03-17 20:44:26 -0400

    add: javascript

--- rps/controller.js
+++ rps/controller.js
@@ -4,21 +4,33 @@ var game = new Game();
 // main entry point
 function playGame() {
     //*********************************ADD CODE HERE *************************************/
-    // Call the function game.newGame() to reset the counts.
+    game.newGame();
 
     //*********************************ADD CODE HERE *************************************/
     // Get the number of rounds from the user, and store it in a variable called numberOfRounds.
-    // Use game.IsNumerOfRoundsValid() to validate the user input.
+    let validInput = false;
+    while(!validInput) {
+        let numberOfRounds = prompt("How many rounds are you going to play");
+        // Use game.IsNumerOfRoundsValid() to validate the user input.
+        if(game.isNumberOfRoundsValid(numberOfRounds)){
+            game.setNumberOfRounds(numberOfRounds);
+            validInput = true;
+        }
+    }
     // If the input is valid, use game.setNumberOfRounds(numberOfRounds) to set the number of rounds for the game 
-    //  >> Else, try to get the number of rounds for the suer again
+    //  >> Else, try to get the number of rounds for the user again
 
 
     //*********************************ADD CODE HERE *************************************/
     // Call playRound() with a parameter of the round count (1 thru numberOfRounds) for each round 
-
+    playRound(game.numberOfRounds);
 
     //*********************************ADD CODE HERE *************************************/
     // Display the Final Score from the properties stored in the Game object
+    console.log("The final score is: <br>")
+    console.log("Number of computer wins ${game.countOfComputerWins}");
+    console.log("Number of player wins ${game.countOfPlayerWins}");
+    console.log("Number of ties ${game.countOfTies}");
 
 }
 
@@ -29,6 +41,7 @@ function playGame() {
 function playRound(roundNumber) {
     //*********************************ADD CODE HERE *************************************/
     // Display the Current Score to the User
+    console.log("The current score is <br> Computer: ${game.countOfComputerWins}, <br> Player: ${game.countOfPLayerwins} <br> Ties: ${game.countOfTies}");
     
     //Prompt for Player Selection
     var playerSelection = getPlayerSelection();
@@ -52,5 +65,15 @@ function getPlayerSelection() {
     
     //*********************************ADD CODE HERE *************************************/
     // Prompt, Validate and Return the player selection. This must be in the form of "rock", "paper", or "scissors" exactly
+    while(!validInput){
+        let playerSelection = prompt("Make your selection (must be 'rock', 'paper', or 'scissors'");
+        switch(playerSelection) {
+            case 'rock' || 'paper' || 'scissors':
+                validInput = true;
+            default:
+                console.log("Invalid selection (must be 'rock', 'paper', or 'scissors'");
+        }
+    }
+
 }
 
